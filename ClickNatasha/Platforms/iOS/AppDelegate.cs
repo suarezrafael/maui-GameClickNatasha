aa45fa5ad2bc8914ded4337eaf0baa8cd5ab9bce
Rafael Vieira Suarez <rafael.suarez@compasso.com.br>
2023-04-19 21:41:57 -0300

    first commit

---
+++ ClickNatasha/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ClickNatasha;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
